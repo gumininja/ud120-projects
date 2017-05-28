0d499059731568b4d32d121acd7b800c87373afa
Attila Mravik <evik.gm@gmail.com>
2017-05-28 23:12:54 +0200

    implement default adaboost and random forest to compare learning time and accuracy

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -5,6 +5,7 @@ matplotlib.use('pdf')
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -32,6 +33,20 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.ensemble import AdaBoostClassifier
+clf = AdaBoostClassifier()
+print "AdaBoost classifier"
+# from sklearn.ensemble import RandomForestClassifier
+# clf = RandomForestClassifier()
+# print "RandomForest classifier"
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+print "prediction time:", round(time()-t0, 3), "s"
+print "accuracy:", accuracy_score(labels_test, pred)
 
 
 
